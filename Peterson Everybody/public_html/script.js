a04ada24cf7341066661d74911f2f9068e032711
dschadlich <dschadlich@gmail.com>
2016-05-11 10:36:24 -0400

    fixed scrolling of the contact in the navbar

--- Peterson Everybody/public_html/script.js
+++ Peterson Everybody/public_html/script.js
@@ -2,3 +2,11 @@ function scroll(){
     scrollBy(0,-135);
     return;
 }
+function chooseContact (){
+  if (window.innerWidth < 750){
+      setTimeout ("scrollBy (0, document.getElementById('leftmail').offsetTop)", 50);
+  }else{
+    setTimeout ("scrollBy (0, document.getElementById('rightmail').offsetTop-135)", 50);
+  }
+
+}
